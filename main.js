8bae0a01f019b996a05206742c5526c1c1754834
sara.mcguinn <sara.mcguinn@gmail.com>
2020-08-09 22:41:37 -0500

    Local storage functions, clear button, delete buttons; styling

--- main.js
+++ main.js
@@ -1,40 +1,93 @@
 var today = moment().format("MMM D, YYYY");
 var cityArray = [];
 var city;
-var cityID;
 var UVIndex;
 var tempK;
 
-$("#currentDate").text(today);
+initialize();
 
-$("#citySearchBtn").on("click", function(event) {
+function initialize() {
+    $("#currentDate").text(today);
+    var lastCity = localStorage.getItem("lastCity");
+    if (lastCity !== null) {
+        city = lastCity
+    } else {
+        city = "Chicago";
+    }
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    if (storedCities !== null) {
+        cityArray = storedCities;
+        renderCityButtons();
+    }
+    getAllWeather();
+}
+
+$("#citySearchBtn").on("click", function (event) {
     event.preventDefault();
-    var userInput = $("#cityInput").val().trim();
-    cityArray.push(userInput);
-    city = userInput;
+    city = $("#cityInput").val().trim();
+    cityArray.push(city);
+    localStorage.setItem("cities", JSON.stringify(cityArray));
+    localStorage.setItem("lastCity", city);
     renderCityButtons();
+    getAllWeather();
+    $("#cityInput").val("");
+})
+
+$("#clearListBtn").on("click", function (event) {
+    event.preventDefault();
+    cityArray = [];
+    $("#cityButtonArea").empty();
+    localStorage.clear();
+})
+
+$(document).on("click", ".cityBtn", function (event) {
+    event.preventDefault();
+    city = $(this).attr("id");
+    localStorage.setItem("lastCity", city);
     getAllWeather();
 })
 
-$(document).on("click", ".cityBtn", function(event) {
+$(document).on("click", ".deleteBtn", function (event) {
     event.preventDefault();
-    city = $(this).attr("id");
-    getAllWeather();
+    $(this).parent().remove();
+    var index = cityArray.indexOf($(this).parent().attr("id"));
+    cityArray.splice(index, 1);
+    localStorage.setItem("cities", JSON.stringify(cityArray));
+    renderCityButtons();
+    event.stopPropogation();
+})
+
+$(document).ajaxError(function () {
+    $("#errorModal").modal();
+    cityArray.splice((cityArray.length - 1), 1);
+    localStorage.setItem("cities", JSON.stringify(cityArray));
+    if (cityArray.length > 0) {
+        localStorage.setItem("lastCity", cityArray[0]);
+    }else {
+        localStorage.setItem("lastCity", "Chicago");
+    }
+    renderCityButtons();
 })
 
 function renderCityButtons() {
     $("#cityButtonArea").empty();
-    for (var i=0; i<cityArray.length; i++) {
+    cityArray = JSON.parse(localStorage.getItem("cities"));
+    for (var i = 0; i < cityArray.length; i++) {
         var newButton = $("<button>");
+        var icon = $("<i>");
+        icon.addClass("fas fa-window-close deleteBtn");
         newButton.text(cityArray[i]);
         newButton.addClass("cityBtn");
+        newButton.addClass("btn");
+        newButton.addClass("btn-dark");
         newButton.attr("id", cityArray[i]);
+        newButton.append(icon);
         $("#cityButtonArea").append(newButton);
     }
 }
 
 function convertTemp(tempK) {
-    return((tempK - 273.15) * 1.80 + 32);
+    return ((tempK - 273.15) * 1.80 + 32);
 }
 
 function rateUVIndex() {
@@ -42,11 +95,11 @@ function rateUVIndex() {
         $("#currentUV").addClass("UVLow");
         $("#currentUV").removeClass("UVHigh");
         $("#currentUV").removeClass("UVModerate");
-    }else if (UVIndex > 7) {
+    } else if (UVIndex > 7) {
         $("#currentUV").addClass("UVHigh");
         $("#currentUV").removeClass("UVModerate");
         $("#currentUV").removeClass("UVLow");
-    }else {
+    } else {
         $("#currentUV").addClass("UVModerate");
         $("#currentUV").removeClass("UVHigh");
         $("#currentUV").removeClass("UVLow");
@@ -55,11 +108,11 @@ function rateUVIndex() {
 
 function getAllWeather() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=354bfca1995e10eb413ecf3fc6ff2b3f";
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         $("#cityName").text(response.name);
         var lat = response.coord.lat;
         var lon = response.coord.lon;
@@ -67,10 +120,10 @@ function getAllWeather() {
         $.ajax({
             url: nextQueryURL,
             method: "GET"
-        }).then(function(response){
+        }).then(function (response) {
             tempK = response.current.temp;
-            var iconCode = response.current.weather[0].icon;
-            var currentIconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
+            var currentIconCode = response.current.weather[0].icon;
+            var currentIconURL = "https://openweathermap.org/img/w/" + currentIconCode + ".png";
             $("#currentIcon").attr("src", currentIconURL);
             $("#currentTemp").text(parseInt(convertTemp(tempK)));
             $("#currentHumidity").text(response.current.humidity);
@@ -78,14 +131,14 @@ function getAllWeather() {
             UVIndex = response.current.uvi;
             $("#currentUV").text(UVIndex);
             rateUVIndex();
-            for (var i=1; i<6; i++) {
-                var iconCode = response.daily[i].weather[0].icon;
-                var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
-                $("#icon"+i).attr("src", iconURL);
-                $("#date"+i).text(moment().add(i,"d").format("M[/]D[/]YY"));
+            for (var i = 1; i < 6; i++) {
+                var dailyIconCode = response.daily[i].weather[0].icon;
+                var dailyIconURL = "https://openweathermap.org/img/w/" + dailyIconCode + ".png";
+                $("#icon" + i).attr("src", dailyIconURL);
+                $("#date" + i).text(moment().add(i, "d").format("M[/]D[/]YY"));
                 tempK = (response.daily[i].temp.day);
-                $("#forecast"+i+"Temp").text(parseInt(convertTemp(tempK)));
-                $("#forecast"+i+"Hum").text(response.daily[i].humidity);
+                $("#forecast" + i + "Temp").text(parseInt(convertTemp(tempK)));
+                $("#forecast" + i + "Hum").text(response.daily[i].humidity);
             }
         })
     });
